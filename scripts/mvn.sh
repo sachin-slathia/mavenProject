b1a1938d73641f4140204f391351bca29e10d8c4
Slathia <slathiasachin13@gmail.com>
2018-12-27 17:20:47 +0530

    Deleted script folder

--- scripts/mvn.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-COMPILE_ARGS="compile"
-TEST_ARGS="test"
-CHECK_STYLE="checkstyle:checkstyle"
-PMD_ARGS="pmd:pmd"
-
-mvn clean $COMPILE_ARGS $CHECK_STYLE $TEST_ARGS $PMD_ARGS
-
